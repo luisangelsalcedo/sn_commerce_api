7923b1c4b2439eaf05dc4c4efe66b8e5ed253685
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-08-11 21:34:31 -0500

    Start Project SringBoot

---
+++ src/main/java/com/cibertec/commerceapi/CommerceapiApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.commerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceapiApplication.class, args);
+	}
+
+}
