bad23133d636033cee156643061fe21d41fc7e2a
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-08-11 23:52:57 -0500

    Fix: Add decorator @GeneratedValue in Cliente class

--- src/main/java/com/cibertec/commerceapi/model/Cliente.java
+++ src/main/java/com/cibertec/commerceapi/model/Cliente.java
@@ -17,6 +17,7 @@ import java.util.Date;
 public class Cliente {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_cliente", nullable = false)
     private Long idCliente;
 
