d40587f9b6cc9590d0a6e4f7a899fc78132efc1e
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-08-11 22:16:48 -0500

    Feat: Add relation between Cliente and TipoDocIdentidad

--- src/main/java/com/cibertec/commerceapi/model/Cliente.java
+++ src/main/java/com/cibertec/commerceapi/model/Cliente.java
@@ -1,10 +1,19 @@
 package com.cibertec.commerceapi.model;
 
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name="tb_cliente")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
     @Column(name="id_cliente", nullable = false)
     private Long idCliente;
@@ -17,9 +26,9 @@ public class Cliente {
 
     @OneToOne
     @JoinColumn(name="id_usuario")
-    private Long idUsuario;
+    private Long idUsuario; // dato de microservicio externo
 
-//    @OneToOne
-//    @JoinColumn(name="id_tipo_doc_identidad")
-//    private TipoDocIdentidad idTipoDocIdentidad;
+    @OneToOne
+    @JoinColumn(name="id_tipo_doc_identidad")
+    private TipoDocIdentidad idTipoDocIdentidad;
 }
