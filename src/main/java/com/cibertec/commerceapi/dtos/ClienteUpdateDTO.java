653bb5a0583fc20513ce0e95add9d6fe3c980d1c
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-08-11 22:47:44 -0500

    Feat: Add DTO class for Clientes

---
+++ src/main/java/com/cibertec/commerceapi/dtos/ClienteUpdateDTO.java
@@ -0,0 +1,17 @@
+package com.cibertec.commerceapi.dtos;
+
+import com.cibertec.commerceapi.model.TipoDocIdentidad;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ClienteUpdateDTO {
+    private Long idCliente;
+    private Date fecNacimiento;
+    private String numDocIdentidad;
+    private Long idUsuario; // dato de microservicio externo
+    private TipoDocIdentidad idTipoDocIdentidad;
+}
